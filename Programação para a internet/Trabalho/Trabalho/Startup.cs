258148372a441b9f30a5d7aac216e30f6ad189f5
Andre Pedro <ndr.pdro@gmail.com>
2017-11-23 22:42:11 +0000

    Alteração da pagina index
    Criaçao de slides, inserir imagens e melhoramento do layout

--- Programação para a internet/Trabalho/Trabalho/Startup.cs
+++ Programação para a internet/Trabalho/Trabalho/Startup.cs
@@ -72,7 +72,7 @@ namespace Trabalho
                     template: "{controller=Home}/{action=Index}/{id?}");
             });
 
-            SeedData.EnsurePopulated(app.ApplicationServices);
+            //SeedData.EnsurePopulated(app.ApplicationServices);
         }
     }
 }
