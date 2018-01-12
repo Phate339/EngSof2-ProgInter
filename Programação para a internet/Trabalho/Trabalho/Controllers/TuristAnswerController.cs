4e6d6ff6a38c3eaa2fcc4a5d55d8f7f81a2f0338
Andre Pedro <ndr.pdro@gmail.com>
2018-01-12 11:01:05 +0000

    Criação de Controlador Para os Questionarios e Criação da View

---
+++ Programação para a internet/Trabalho/Trabalho/Controllers/TuristAnswerController.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Trabalho.Models;
+
+namespace Trabalho.Controllers
+{
+    public class TuristAnswerController : Controller
+    {
+        private readonly TrabalhoDbContext _context;
+
+        public TuristAnswerController(TrabalhoDbContext context)
+        {
+            _context = context;
+        }
+       
+        public async Task<IActionResult> Index()
+            {
+                var typeAnswer = _context.TypeAnswer.Include(t => t.Questions).AsNoTracking();
+
+                return View(await typeAnswer.ToListAsync());
+        }
+    }
+}
